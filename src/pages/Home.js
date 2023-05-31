c1c1b11152e55d195d4ac56b55291f8606ea4785
Vikramsinh28 <vikramsinhparmar2812@gmail.com>
2023-05-31 17:55:38 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Navbar from '../layout/Navbar'
+
+
+const DashboardChart = () => {
+    return (
+        <>
+            <Navbar />
+            <Box height={64 + 'px'} />
+            <Box sx={{ display: 'flex' }}>
+                
+            </Box>
+        </>
+    )
+}
+
+export default DashboardChart
---
+++ src/pages/Login.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { TextField,  Button, Container , Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (values) => {
+
+       try{
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            const users = response.data;
+            const user = users.find((user) => 
+                user.email === values.email && user.phone === values.password
+            );
+    
+            if(user) {
+                alert("Loggin in!")
+                navigate('/')
+            }else {
+                alert("not valid")
+            }
+       } 
+       catch(err) {
+           console.log('error' , err)
+       }
+
+    } 
+
+    const validationSchema = yup.object({
+        email: yup.string().required('Username is required'),
+        password: yup.string().required('Password is required'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email : '',
+            password : '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: values => {
+            handleSubmit(values)
+        }
+    });
+
+    return (
+    <Container maxWidth="xs" sx={{
+        marginTop: 8,
+    }}>
+      <Typography variant="h4" align="center" gutterBottom>
+         Login 
+      </Typography>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          margin="normal"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          margin="normal"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          
+        />
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+        >
+          Login
+        </Button>
+      </form>
+    </Container>
+    )
+}
+
+export default Login;
